7ea2fc7b2663964b5d81199089b363f61f0cd429
kay019 <crowruya@gmail.com>
2023-02-19 23:35:56 +0900

    create project

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,56 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args){
+
+        //일괄적인 한 단위 작업마다 생성해야 한다.
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        //트랜잭션 호출
+        //jpa 모든 작업은 트랜잭션 안에서 작업
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        // 정석
+        try{
+            //code
+
+            /*
+            //insert
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+            em.persist(member);
+            */
+
+            //update
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("find Member.id = " + findMember.getId());
+            System.out.println("find Member.name = " + findMember.getName());
+
+            findMember.setName("HelloJPA");
+
+            //delete
+            //em.remove(findMember);
+
+            tx.commit();
+
+        }catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
+
+    }
+}
